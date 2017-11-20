5c496b89dcf8540f0ac5020e74dcadf682d0779d
raj594 <rajaynes@gmail.com>
2017-11-19 22:53:00 -0600

    fix some handlebars rendering and expense searching by user

--- routes/visualization-api-routes.js
+++ routes/visualization-api-routes.js
@@ -8,8 +8,8 @@ var db = require("../models");
 module.exports = function(app) {
   //index route loads view.html
 
-	app.get("/api/expenses", function(req, res) {
-		db.Expense.findAll({
+	app.get("/api/expenses/:user", function(req, res) {
+		db.Expense.findAll({ where: { user_name: req.params.user }
 			//here is where we will add the information to search the expenses by
 			//for instance, add a /api/expenses/:XXXXXX to the request and use req.params or add it into a form and use req.body.name
 			//then also also add a "where: name=XXXXXX" in this spot.
@@ -18,7 +18,6 @@ module.exports = function(app) {
 		      var hbsObject = {
 		        expense: data
 		      };
-		      // res.render("d3Visualization", hbsObject);
 		      // return to the ajax call a json object containing all of the expenses from the user entered in the form.
 		      res.json(data)
 		    });
