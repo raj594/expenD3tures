9c53e907e9c27856dca2374ef913ae6b123efe00
raj594 <rajaynes@gmail.com>
2017-11-22 13:30:46 -0600

    change expenses models to prepare to tie it to the users model

--- models/expenses.js
+++ models/expenses.js
@@ -1,11 +1,32 @@
+        // id: {
+        //     autoIncrement: true,
+        //     primaryKey: true,
+        //     type: Sequelize.INTEGER
+        // },
+
+
 module.exports = function (sequelize, DataTypes) {
 	var Expense = sequelize.define("Expense", {
-		user_name: DataTypes.STRING,
-		expense: DataTypes.BOOLEAN,
-		expense_category: DataTypes.STRING,
-		expense_value: DataTypes.NUMERIC(6,2),
-		recurring: DataTypes.BOOLEAN,
-		recurring_periodic: DataTypes.INTEGER
+		user_name: {
+			type: DataTypes.STRING,
+		},
+
+		expense: {
+			type: DataTypes.BOOLEAN,
+		},
+
+		expense_category: {
+			type: DataTypes.STRING,
+		},
+		expense_value: {
+			type: DataTypes.NUMERIC(6,2),
+		},
+		recurring: {
+			type: DataTypes.BOOLEAN,
+		},
+		recurring_periodic: {
+			type: DataTypes.INTEGER
+		}
 	});
 	return Expense
 };
